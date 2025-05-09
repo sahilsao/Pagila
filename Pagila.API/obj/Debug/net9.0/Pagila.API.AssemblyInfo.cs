a3acb086ea62b42d29e0a75416276849fe8787ef
sahilsao <sahilsaocs@gmail.com>
2025-05-09 20:17:01 +0530

    fix

--- Pagila.API/obj/Debug/net9.0/Pagila.API.AssemblyInfo.cs
+++ Pagila.API/obj/Debug/net9.0/Pagila.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pagila.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa0c1e9ea6aa8d1ef820e1e61240f42c0e4c896")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a2ef10c39708a75ade0af6412e4904ce636aed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pagila.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pagila.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
