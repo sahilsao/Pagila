0e3b96a52ed13202fb40d3a9a7917d40850e03f9
sahilsao <sahilsaocs@gmail.com>
2025-05-09 20:24:25 +0530

    fix all files added

---
+++ Pagila.API/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Pagila.API.Configurations;
+using Pagila.API.Data;
+using Pagila.API.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<PagilaDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+ builder.Services.AddOpenApiConfiguration(builder.Environment);   
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseOpenApiConfiguration();
+
+app.UseHttpsRedirection();
+
+// Register endpoints
+app.MapFilmEndpoints();
+
+app.Run();
+
